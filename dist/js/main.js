95f75b1c7f190563799bd3bd52670aeec2286a58
Sommelier <Arty.03.07@gmail.com>
2024-06-28 03:38:21 +0300

    Upgrade main slider and adding animate

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,137 +1,182 @@
-let mainSlider = $('.main-slider');
+let mainSliderJQ = $('.main-slider');
+let mainSlider = document.querySelector('.main-slider');
 
-mainSlider.slick({
+mainSliderJQ.slick({
     dots: true,
     arrows: true,
     infinite: true,
     slidesToShow: 1,
     fade: true,
     cssEase: 'linear'
-    // adaptiveHeight: true,
 });
 
 let logo = document.querySelector('.logo');
-let mainSliderSlick = mainSlider.slick('getSlick');
-let mainArrows = mainSlider.children('.slick-arrow');
-let mainDots = mainSliderSlick?.$dots[0];
+let mainSliderSlick = mainSliderJQ.slick('getSlick');
 let mainFirstSlide = mainSliderSlick?.$slides[0];
-let mainOffset = mainFirstSlide?.querySelector('.button')?.offsetTop;
 
-let getOffsetLeft = sel => {
-    let offset = sel.offsetLeft;
+let mainArrows = document.querySelectorAll('.slick-arrow');
+let mainDots = mainSlider.querySelector('.slick-dots');
+let mainText = mainSlider.querySelector('.slick-active')
+    ?.querySelector('.texts-block');
 
-    if (!offset)
-        return getOffsetLeft(sel?.offsetParent);
-
-    return offset;
+let mobile = {
+    default: {
+        dots: '161px',
+        arrows: '54px',
+    },
+    switch: {
+        ...this.default
+    },
+    resize: {
+        ...this.default
+    },
 };
 
-// Change this
-let offsetArrows = 167; // px
-let offsetDots = 85; // px
-let mainTopSet;
+let desktop = {
+    default: {
+        dots: '80px',
+        arrows: '54px',
+    },
+    switch: {
+        ...this.default
+    },
+    resize: {
+        ...this.default
+    },
+};
 
-// After init show arrows and dots
-if (mainOffset){
-    mainTopSet = mainOffset + offsetDots;
-    mainArrows.css({
-        "opacity": 1,
-        "top": mainOffset + offsetArrows
-    });
+let getOffsetLeft = selector => {
+    if (!selector)
+        return selector
 
-    if (window.innerWidth <= 760){
-        mainDots.style.cssText = `top: ${mainTopSet}px; opacity: 1;`;
-        
+    if (selector.offsetLeft)
+        return selector.offsetLeft;
+    else if (selector && !selector.offsetLeft)
+        return getOffsetLeft(selector?.offsetParent);
+};
+
+let getOffsetTop = selector => {
+    if (!selector)
+        return selector
+    
+    if (selector.offsetTop)
+        return selector.offsetTop;
+    else if (selector && !selector.offsetTop)
+        return getOffsetTop(selector?.offsetParent);
+};
+
+let calculateProperty = (currentProperty, newProperty) => {
+    if (typeof newProperty == 'string'){
+        currentValue = parseFloat(currentProperty);
+        newValue = parseFloat(newProperty.slice(2, -2));
+
+        if (['auto', 'none', 'inherit'].includes(newProperty) || /%$/g.test(newProperty)){
+            return newProperty;
+        } else if (currentValue && newProperty.startsWith('+=')){
+            return `${currentValue - newValue}px`;
+        } else if (currentValue && newProperty.startsWith('-=')){
+            return `${currentValue - newValue}px`;
+        }
+
+        let result = parseFloat(newProperty) || 0;
+        return `${result}px`;
+    }
+};
+
+let setCSS = (element, properties) => {
+    for (let property in properties){
+        currentStyleValue = getComputedStyle(element)[property];
+        element.style[property] = `${calculateProperty(currentStyleValue, properties[property])}`;
+    }
+};
+
+function protoCSS(properties){
+    Array.from(this).forEach(el => setCSS(el, properties));
+}
+function customCSS(properties){
+    setCSS(this, properties);
+}
+NodeList.prototype.css = protoCSS;
+HTMLUListElement.prototype.css = customCSS;
+
+mainFirstSlide.querySelectorAll('.animate__animated')?.forEach(
+    el => el.classList.add('animate__fadeInUp')
+);
+function resize(){
+    mainArrows = document.querySelectorAll('.slick-arrow');
+    mainDots = mainSlider.querySelector('.slick-dots');
+    mainText = mainSlider.querySelector('.slick-active')
+        ?.querySelector('.texts-block');
+
+    // Инициализация стрелок и точек после загрузки.
+    if (window.innerWidth < 760){
+        mainDots.style.cssText = '';
+        mainDots.css({
+            top: 'auto',
+            left: `-=${getOffsetLeft(mainText)-(mainText.querySelector('.button').innerWidth/2)}px`,
+            bottom: mobile.default.dots,
+        });
+
+        mainArrows.forEach(el => el.style.cssText = '');
         mainArrows.css({
-            "opacity": 1,
-            "top": mainOffset + offsetArrows
+            top: 'auto',
+            left: `-=${getOffsetLeft(mainText)-(mainText.querySelector('.button').innerWidth/2)}px`,
+            bottom: mobile.default.arrows
+        });
+
+    } else if (window.innerWidth >= 1280){
+        let offsetTextArrows = 10;
+        let offsetTextDots = 21;
+        
+        mainDots.css({
+            top: 'auto',
+            bottom: desktop.default.dots,
+            left: `-=${getOffsetLeft(mainDots)-getOffsetLeft(mainText)-offsetTextDots}px`
+        });
+    
+        mainArrows.css({
+            top: `${getOffsetTop(mainText.querySelector('.h1')) + offsetTextArrows}px`,
+            left: `-=${getOffsetLeft(mainArrows[0])-getOffsetLeft(logo)}px`,
+            bottom: 'none',
         });
     } else {
-        let textOffset = getOffsetLeft(mainFirstSlide.querySelector('.texts-block'));
-        mainDots.style.cssText = `bottom: 160px; left: ${textOffset+30}px; margin: 0; opacity: 1;`;
-
-        if (window.innerWidth >= 1280){
-            mainDots.style.cssText = `bottom: 80px; left: ${textOffset+30}px; margin: 0; opacity: 1;`;
-            mainArrows.css({
-                "top": 270,
-                "left": `-=${getOffsetLeft(mainArrows[0])-getOffsetLeft(logo)}`,
-            });
-        } else {
-            mainArrows.css({
-                "top": mainOffset + offsetArrows,
-                "left": `-=${getOffsetLeft(mainArrows[0])-textOffset-9}`
-            });
-        }
+        let offsetTextArrows = 0;
+        let offsetTopArrows = 10;
+        let offsetTextDots = 21;
+        
+        mainDots.css({
+            top: 'auto',
+            bottom: desktop.default.dots,
+            left: `-=${getOffsetLeft(mainDots)-getOffsetLeft(mainText)-offsetTextDots}px`
+        });
+    
+        mainArrows.css({
+            top: 'auto',
+            bottom: `${120+offsetTopArrows}px`,
+            left: `-=${getOffsetLeft(mainArrows[0])-getOffsetLeft(mainText)-offsetTextArrows}px`,
+        });
     }
 }
+resize();
+window.addEventListener('resize', resize);
 
-mainSlider.on('beforeChange', (slick, current, next) => {
-    mainDots.style.cssText = `opacity: 0;`;
-    mainArrows.css("opacity", 0);
+// Выполнение перед изменением слайда.
+mainSliderJQ.on('beforeChange', (slick, current, next) => {
+    let animateName = 'animate__fadeInUp';
+    let currentSlide = current.$slides[next];
+    let currentSelectors = currentSlide.querySelectorAll('.animate__animated');
+    currentSelectors.forEach(el => el.classList.remove(animateName));
 });
 
-mainSlider.on('afterChange', (slick, current) => {
-    let slide = current.$slides[current.currentSlide];
-    let currentSlideHeight = slide.querySelector('.button')?.offsetTop;
-    let mainTopSet = currentSlideHeight + offsetDots;
-    
-    if (window.innerWidth <= 760){
-        mainArrows.css({
-            "opacity": 1,
-            "top": currentSlideHeight + offsetArrows
-        });
-        mainDots.style.cssText = `opacity: 1; bottom: 140px;`;
-    } else {
-        let textOffset = getOffsetLeft(mainFirstSlide.querySelector('.texts-block'));
-        mainDots.style.cssText = `bottom: 160px; left: ${textOffset+30}px; margin: 0; opacity: 1;`;
-
-        if (window.innerWidth >= 1280){
-            mainDots.style.cssText = `bottom: 80px; left: ${textOffset+30}px; margin: 0; opacity: 1;`;
-            mainArrows.css({
-                "opacity": 1,
-                "top": 270,
-                "left": `-=${getOffsetLeft(mainArrows[0])-getOffsetLeft(logo)-8}`
-            });
-        } else {
-            mainArrows.css({
-                "opacity": 1,
-                "top": currentSlideHeight + offsetArrows
-            });
-        }
-    }
-});
-
-window.addEventListener("resize", event => {
-    let mainTextBlock = document.querySelector('.slick-active');
-    let mainTextPosition = mainTextBlock.querySelector('.texts-block');
-    let offsetButton = mainTextBlock.querySelector('.button').offsetTop;
-    let mainTopSet = offsetButton + offsetDots;
-    let offsetArrow = offsetButton + offsetArrows;
-    
-    if (window.innerWidth <= 760){
-        mainDots.style.cssText = `opacity: 1; top: ${mainTopSet}px`;
-        mainArrows.css({
-            "opacity": 1,
-            "top": offsetArrow,
-            "left": ""
-        });
-    } else {
-        let textOffset = getOffsetLeft(mainTextPosition);
-        
-        if (window.innerWidth >= 1280){
-            mainDots.style.cssText = `top: ${mainTopSet+95}px; left: ${textOffset}px; margin: 0; opacity: 1;`;
-            mainArrows.css({
-                "top": 270,
-                "left": `-=${getOffsetLeft(mainArrows[0])-getOffsetLeft(logo)}`
-            });
-        } else {
-            mainArrows.css({
-                "top": offsetArrow,
-                "left": `-=${getOffsetLeft(mainArrows[0])-textOffset-8}`
-            });
-        }
-    }
-});
+mainArrows.forEach(
+    arrow => arrow.addEventListener('click', event => {
+        let animateName = 'animate__fadeInUp';
+        let slick = mainSliderJQ.slick('getSlick');
+        let currentSlide = slick.$slides[slick.currentSlide];
+        let currentSelectors = currentSlide.querySelectorAll('.animate__animated');
+        currentSelectors.forEach(el => el.classList.add(animateName));
+    })
+);
 
 // $('.selector').slick({
 //     nextArrow: '<i class="fa fa-arrow-right"></i>',
