245508e129474a0379a7d25d0abd0fdee24337ab
Asya <web.zaslavskaya@gmail.com>
2024-07-05 17:48:33 +0300

    news page

--- dist/js/news.js
+++ dist/js/news.js
@@ -38,92 +38,92 @@ function updateDisplay() {
   // Додаємо слухача події на зміну розміру вікна
   window.addEventListener('resize', updateDisplay);
 
-  let screenWidth = window.innerWidth;
-  let paginationArray = document.querySelectorAll('.num');
+//   let screenWidth = window.innerWidth;
+//   let paginationArray = document.querySelectorAll('.num');
   
-  // Перевіряємо правильність отримання елементів
-  console.log(paginationArray);
+//   // Перевіряємо правильність отримання елементів
+//   console.log(paginationArray);
   
-  // Проходимо по кожному елементу
-  paginationArray.forEach(element => {
-    let targetSlide = parseInt(element.dataset.targetslide); // Перетворюємо на число
-    console.log(`data-targetSlide: ${targetSlide}`);
+//   // Проходимо по кожному елементу
+//   paginationArray.forEach(element => {
+//     let targetSlide = parseInt(element.dataset.targetslide); // Перетворюємо на число
+//     console.log(`data-targetSlide: ${targetSlide}`);
     
   
-    element.addEventListener("click", (event) => {
-      console.log(event.currentTarget.dataset.targetslide);
+//     element.addEventListener("click", (event) => {
+//       console.log(event.currentTarget.dataset.targetslide);
 
-      paginationArray.forEach((el)=> {
-          el.classList.remove('active')
-      })
+//       paginationArray.forEach((el)=> {
+//           el.classList.remove('active')
+//       })
 
-      event.currentTarget.classList.add('active');
+//       event.currentTarget.classList.add('active');
   
-      if (screenWidth < 760) {
-        switch (targetSlide) {
-          case 1:
-            for (let i = 0; i < newsArray.length; i++) {
-              newsArray[i].style.display = 'none';
-            }
-            newsArray[0].style.display = 'block';
-            newsArray[1].style.display = 'block';
-            newsArray[2].style.display = 'block';
-            break;
-          case 2:
-            for (let i = 0; i < newsArray.length; i++) {
-              newsArray[i].style.display = 'none';
-            }
-            newsArray[3].style.display = 'block';
-            newsArray[4].style.display = 'block';
-            newsArray[5].style.display = 'block';
-            break;
-          case 10:
-            for (let i = 0; i < newsArray.length; i++) {
-              newsArray[i].style.display = 'none';
-            }
-            newsArray[6].style.display = 'block';
-            newsArray[7].style.display = 'block';
-            newsArray[8].style.display = 'block';
-            break;
-          default:
-            // Інші випадки
-            break;
-        }
-      } else{
-        switch (targetSlide) {
-            case 1:
-              for (let i = 0; i < newsArray.length; i++) {
-                newsArray[i].style.display = 'none';
-              }
-              for (let i = 0; i < 9; i++) {
-                newsArray[i].style.display = 'block';
-              }
-              break;
-            case 2:
-                for (let i = 0; i < newsArray.length; i++) {
-                    newsArray[i].style.display = 'none';
-                  }
-                  for (let i = 9; i < 18; i++) {
-                    newsArray[i].style.display = 'block';
-                  }
-              break;
-            case 10:
-                for (let i = 0; i < newsArray.length; i++) {
-                    newsArray[i].style.display = 'none';
-                  }
-                  for (let i = 18; i < 27; i++) {
-                    newsArray[i].style.display = 'block';
-                  }
-              break;
-            default:
-              // Інші випадки
-              break;
-      }
-    };
-    });
+//       if (screenWidth < 760) {
+//         switch (targetSlide) {
+//           case 1:
+//             for (let i = 0; i < newsArray.length; i++) {
+//               newsArray[i].style.display = 'none';
+//             }
+//             newsArray[0].style.display = 'block';
+//             newsArray[1].style.display = 'block';
+//             newsArray[2].style.display = 'block';
+//             break;
+//           case 2:
+//             for (let i = 0; i < newsArray.length; i++) {
+//               newsArray[i].style.display = 'none';
+//             }
+//             newsArray[3].style.display = 'block';
+//             newsArray[4].style.display = 'block';
+//             newsArray[5].style.display = 'block';
+//             break;
+//           case 10:
+//             for (let i = 0; i < newsArray.length; i++) {
+//               newsArray[i].style.display = 'none';
+//             }
+//             newsArray[6].style.display = 'block';
+//             newsArray[7].style.display = 'block';
+//             newsArray[8].style.display = 'block';
+//             break;
+//           default:
+//             // Інші випадки
+//             break;
+//         }
+//       } else{
+//         switch (targetSlide) {
+//             case 1:
+//               for (let i = 0; i < newsArray.length; i++) {
+//                 newsArray[i].style.display = 'none';
+//               }
+//               for (let i = 0; i < 9; i++) {
+//                 newsArray[i].style.display = 'block';
+//               }
+//               break;
+//             case 2:
+//                 for (let i = 0; i < newsArray.length; i++) {
+//                     newsArray[i].style.display = 'none';
+//                   }
+//                   for (let i = 9; i < 18; i++) {
+//                     newsArray[i].style.display = 'block';
+//                   }
+//               break;
+//             case 10:
+//                 for (let i = 0; i < newsArray.length; i++) {
+//                     newsArray[i].style.display = 'none';
+//                   }
+//                   for (let i = 18; i < 27; i++) {
+//                     newsArray[i].style.display = 'block';
+//                   }
+//               break;
+//             default:
+//               // Інші випадки
+//               break;
+//       }
+//     };
+//     });
 
 
 
-});
+// });
 
 //# sourceMappingURL=news.js.map
