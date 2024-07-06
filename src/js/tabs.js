84a2d89d8fd3d9a2653822a3c2d50fd05719c3b7
Asya <web.zaslavskaya@gmail.com>
2024-07-06 11:57:33 +0300

    price tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -18,6 +18,51 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+
+
+
+// let accI = document.getElementsByClassName("accordionI");
+// let b;     
+
+// for (b = 0; b < accI.length; b++) {
+//   accI[b].addEventListener("click", function() {
+//     this.parentNode.classList.toggle("active");
+//     // console.log(this.parentNode);
+//     // this.closest('.panel').classList.toggle('active');
+//     let panelI = this.nextElementSibling;
+//     if (panelI.style.maxHeight) {
+//       panelI.style.maxHeight = null;
+//     } else {
+//         console.log(panelI.scrollHeight);
+//         console.log(panelI.scrollHeight+ 150);
+
+//       panelI.style.maxHeight = panelI.scrollHeight + 140 + "px";
+//     } 
+//   });
+// }
+
+
+let ac = document.getElementsByClassName("accordi");
+let b;
+
+for (b = 0; b < ac.length; b++) {
+  ac[b].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.parentNode.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let pane = this.nextElementSibling;
+    if (pane.style.display.height === "auto") {
+      pane.style.display.height = "0";
+    } else {
+      pane.style.display.height = "auto";
+    }
+  });
+}
+
+
+
 // .wrap-tab
 //   .accordion.card-link
 //   .panel.lists-wrap
